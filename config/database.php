bdba874e41ce62d727dcda1fe13c5d6d87e9f0ed
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-11-26 20:09:18 +0800

    update

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', 'postgres://ytjikufu:5lbxF_4vIV848mKbpxbnxMazxmrA8JjT@satao.db.elephantsql.com:5432/ytjikufu'),
+            'host' => env('DB_HOST', 'satao.db.elephantsql.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ytjikufu'),
+            'username' => env('DB_USERNAME', 'ytjikufu'),
+            'password' => env('DB_PASSWORD', '5lbxF_4vIV848mKbpxbnxMazxmrA8JjT'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
