005d84119ad5661a16929aff1998f69b31fd99d4
Jamel <jamelyassin84@gmail.com>
2020-12-02 14:41:46 +0800

    'Withdrawals'

--- database/migrations/2020_12_02_061155_create_withdrawals_table.php
+++ database/migrations/2020_12_02_061155_create_withdrawals_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Organization;
+use App\Models\User;
 
 class CreateWithdrawalsTable extends Migration
 {
