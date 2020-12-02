a1878f3e07310eea4ae3a869d1a448dc4f161ab7
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-12-02 13:27:28 +0800

    Update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -24,29 +24,29 @@ class SearchController extends Controller
         if ($request->has('role')) {
             $role = $request->input('role');
 
-            $users = $users->filter(function ($user) use ($role) {
+            $users = new Collection($users->filter(function ($user) use ($role) {
                 foreach ($user->memberships as $membership) {
                     if ($membership->role === $role) {
                         return true;
                     }
                 }
                 return false;
-            });
+            }));
         }
 
         if ($request->has('organization_id')) {
             $organization = Organization::find($request->input('organization_id'));
-            $members = $organization->members()
+            $members = new Collection($organization->members()
                 ->whereIn('user_id', $users->map(function ($user) {
                     return $user->id;
                 })
                     ->all())
                 ->with('user')
-                ->get();
+                ->get());
 
-            $users = $members->map(function ($member) {
+            $users = new Collection($members->map(function ($member) {
                 return $member->user;
-            });
+            }));
         }
 
         return $users;
